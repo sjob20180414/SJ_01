7adaf5744131acc907fb3242ea52c145920e399f
koutarou <matsuikoutarou@kotaroMacBookPro.local>
2018-05-27 17:38:34 +0900

    コメント削除

--- SJ01/View/Home/HomeViewController.swift
+++ SJ01/View/Home/HomeViewController.swift
@@ -5,13 +5,6 @@
 //  Created by Yudai Terashita on 2018/05/27.
 //  Copyright © 2018年 松井康太郎. All rights reserved.
 //
-//
-//  LoginViewController.swift
-//  SJ01
-//
-//  Created by 松井康太郎 on 2018/04/14.
-//  Copyright © 2018年 松井康太郎. All rights reserved.
-//
 
 import Foundation
 import UIKit
